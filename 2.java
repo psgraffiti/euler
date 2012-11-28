e33ec05aab2ef3e7102cbb1d0dffbde0ab8ac0e6
PSGraffiti <rurokenster@gmail.com>
2012-11-27 22:29:43 -0800

    trying to commit new shit

--- 2.java
+++ 2.java
@@ -3,3 +3,14 @@
 
 // By considering the terms in the Fibonacci sequence whose values do not exceed
 // four million, find the sum of the even-valued terms.
+
+import java.util.*;
+
+public class 2 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("HERE");
+	}
+}
+		 
