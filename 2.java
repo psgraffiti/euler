948286387ca5678a0346ec6e45da26015cca76ad
Greg Sandstrom <g.a.t.sandstrom@gmail.com>
2012-11-27 21:44:58 -0800

    problem 2 framework

---
+++ 2.java
@@ -0,0 +1,5 @@
+// problem 2
+// http://projecteuler.net/problem=2
+
+// By considering the terms in the Fibonacci sequence whose values do not exceed
+// four million, find the sum of the even-valued terms.
